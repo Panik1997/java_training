09a8f4a1ccf140d196e708fd50e74492604f0559
Panik1997 <49204020+Panik1997@users.noreply.github.com>
2019-05-02 17:42:45 +0200

    Add files via upload
    Some String, int, Array and List operations. This program creates an Array of a single numbers given by user, using a List.

---
+++ NumbersAndArrays.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.lang.String;
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class NumbersAndArrays {
+
+    private static void ConvertToArray(int size1, List<Integer> firstList1){
+        int k = 0;
+        int[] arrayOfList = new int[size1];
+
+
+        for(Integer x : firstList1){
+            if(k == size1){
+                break;
+            }
+            arrayOfList[k++] = x;
+        }
+
+        System.out.println(Arrays.toString(arrayOfList));
+    }
+
+    private static List<Integer> ConvertToList(int number, int size){
+        List<Integer> firstList = new ArrayList<Integer>();
+
+        for(; number != 0; number /= 10){
+            firstList.add(number % 10);
+        }
+
+        Collections.reverse(firstList);
+
+        return firstList;
+    }
+    public static void main(String[] args) {
+
+            System.out.println("Enter the number");
+            Scanner x = new Scanner(System.in);
+            int y= x.nextInt();
+            System.out.println("You entered a number: " + y);
+
+            int howLong = String.valueOf(y).length();
+
+            System.out.println("List of a single numbers: " + ConvertToList(y, howLong));
+            System.out.print("And now an Array of a single numbers made by using List: ");
+            ConvertToArray(y, ConvertToList(y, howLong));
+    }
+}
